5da5a255bdafd8c1a2d323669dc004781257fd0b
42 VEC Mac 2 <42vecmac2@42s-iMac.local>
2018-03-28 14:40:40 -0700

    kill is not KILLING ok?

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ Creating a [shell with C](https://brennan.io/2015/01/16/write-a-shell-in-c/)
 Objective:
 1. Learn most of the functions from minishell.
   * fork() :white_check_mark:
-  * kill()
+  * kill() :white_check_mark:
   * execve()
   * signal()
-  * kill()
   * access()
   * getcwd()
   * stat()
---
+++ src/main_kill.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_kill.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 42vecmac <42vecmac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/27 12:51:13 by 42vecmac          #+#    #+#             */
+/*   Updated: 2018/03/28 14:40:18 by 42vecmac2        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <sys/types.h>
+
+/*
+** kill() sends any signtal to any process group or single process.
+** At first, I thought it just another way to describe exit(), but is different.
+** It uses the "signal.h" that handles the number of the signal type.
+** 0 SIGNULL	->	Null Check access to pid
+** 1 SIGHUP		->	Hangup Terminate; can be trapped
+** 2 SIGINT		->	Interrupt Terminate; can be trapped
+** 3 SIGQUIT	->	Quit Terminate with core dump; can be trapped
+** 9 SIGKILL	->	Kill Forced termination; cannot be trapped
+** 15 SIGTERM	->	Terminate Terminate; can be trapped
+** 24 SIGSTOP	->	Stop Pause the process; cannot be trapped
+** 25 SIGTSTP	->	Terminal stop Pause the process; can be
+** 26 SIGCONT	->	Continue Run a stopped process
+*/
+
+int main(void)
+{
+
+}
